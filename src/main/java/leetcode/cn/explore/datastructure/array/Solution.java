e59e97f08602a7b31cb5f43139670f03ab9b9905
Digger <pgytao@outlook.com>
2019-02-13 18:05:11 +0800

    update solution

--- src/main/java/leetcode/cn/explore/datastructure/array/Solution.java
+++ src/main/java/leetcode/cn/explore/datastructure/array/Solution.java
@@ -1,10 +1,21 @@
 package leetcode.cn.explore.datastructure.array;
 
+import java.util.Arrays;
+
 /**
  * edited by AndersonKim
  * at 2019/2/12
  */
 public class Solution {
+
+    //动态数组就是arraylist
+
+
+    //至少是其他数字两倍的最大数的下标
+    public int dominantIndex(int[] nums) {
+
+    }
+    //数组中心索引
     public int pivotIndex(int[] nums) {
 
     }
